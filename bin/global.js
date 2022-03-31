33d1f47a4285c4f731e7e4a229730ac0cf97bbeb
Samuel <samuelreyesiglesias@gmail.com>
2022-03-31 15:27:12 -0600

    module 1

---
+++ bin/global.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+let nuev = require("../src/index.js");
+
+nuev.nuevo();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const nuevo = function() {
+    console.log("test new");
+    console.log("second message");
+    console.log("third message");
+}
+
+
+module.exports = { nuevo }
